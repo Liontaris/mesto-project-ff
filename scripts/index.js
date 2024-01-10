4b5de488542d71f957d381885a0600b1889cce57
Liontaris <storycrafter@yandex.ru>
2024-01-11 00:35:12 +0300

    p6 v0.1

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,26 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector('#card-template').content;
+const cardTarget = document.querySelector('.places__list');
+
+const buttonFunction = function (deleteButtonID) {
+    deleteButtonID.addEventListener('click', function () {
+        const parentCard = deleteButtonID.closest('.card');
+        parentCard.remove();
+    });
+};
+
+function createCard (putCardTemplate, putCardTarget, putCardData, putButtonListener) {
+    const cardTemplateClone = putCardTemplate.querySelector('.card').cloneNode(true);
+    const deleteButton = cardTemplateClone.querySelector('.card__delete-button');
+    cardTemplateClone.querySelector('.card__image').src = putCardData.link;
+    cardTemplateClone.querySelector('.card__title').textContent = putCardData.name;
+    putCardTarget.append(cardTemplateClone);
+    putButtonListener(deleteButton);
+};
+
+initialCards.forEach(function(item){
+    createCard(cardTemplate, cardTarget, item, buttonFunction);
+});
